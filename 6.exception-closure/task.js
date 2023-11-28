86552d7b1e2b4c2ed8a755fe420e705decd370ed
admin mary <atanova.mary@gmail.com>
2023-11-28 23:56:26 +0400

    Сompleting a task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(value){
+    if(Number.isNaN(Number.parseFloat(value))){
+        throw new Error("Невалидное значение");
+    } else return Number.parseFloat(value);
+}
+
+function validateCount(value){
+    try{
+        return parseCount(value);
+    } catch (error){
+        return error;
+    }
+}
+
+class Triangle{
+    constructor(sideA, sideB, sideC){
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        if (sideA > sideB + sideC || sideB > sideA + sideC || sideC > sideA + sideB){
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter(){
+        return (this.sideA + this.sideB + this.sideC);
+    }
+
+    get area(){
+        const p = this.perimeter / 2;
+        return Number(((p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)) ** 0.5).toFixed(3));
+    }
+}
+
+function getTriangle(sideA, sideB, sideC) {
+    try {
+        return new Triangle(sideA, sideB, sideC);
+    } catch(error) {
+        return{
+            get perimeter(){
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area(){
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
